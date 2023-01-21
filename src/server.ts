298ff314f6d0b3d99bb6db5d11356745ac1a38ed
CydoEntis <cydoentis@gmail.com>
2023-01-21 17:53:25 -0500

    Added database connection function

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,29 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import { config } from "./config/config";
 
 const app = express();
+
+// Connect To MongoDB.
+async function connectToDB() {
+	try {
+		await mongoose.connect(config.mongo.url, {
+			retryWrites: true,
+			w: "majority",
+		});
+		startServer();
+	} catch (error: any) {}
+}
+
+connectToDB();
+
+function startServer() {
+	app.use((req: Request, res: Response, next: NextFunction) => {
+		next();
+	});
+
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+	app.use(cors());
+}
