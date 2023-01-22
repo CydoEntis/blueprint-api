3adcef33859f499d6e9c0d9a4e1ef59da6901b3f
CydoEntis <cydoentis@gmail.com>
2023-01-21 19:14:44 -0500

    Added user model functions

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,11 @@
 import mongoose, { Document, Schema } from "mongoose";
 
+import { NextFunction } from "express";
+import bcrypt from "bcrypt";
+import { config } from "../config/config";
+import jwt from "jsonwebtoken";
+import { nextTick } from "process";
+
 export interface IUser {
 	username: string;
 	password: string;
@@ -11,19 +17,51 @@ export interface IUser {
 
 export interface IUserModel extends IUser, Document {}
 
-const UserSchema: Schema = new Schema(
+const UserSchema: Schema = new Schema<IUserModel>(
 	{
-		username: { type: String, required: true },
-		password: { type: String, required: true },
-		email: { type: String, required: true },
-		firstName: { type: String, required: true },
-		lastName: { type: String, required: true },
-		avatar: { type: String, required: true },
+		username: { type: String, required: [true, "Please provide a username"] },
+		password: { type: String, required: [true, "Please provide a password"] },
+		email: { type: String, required: [true, "Please provide a valid email"] },
+		firstName: {
+			type: String,
+			required: [true, "Please provide a first name"],
+		},
+		lastName: { type: String, required: [true, "Please provide a last name"] },
+		avatar: { type: String },
 	},
 	{
 		timestamps: true,
 		versionKey: false,
 	},
 );
+
+UserSchema.pre("save", async function (next) {
+	let user = this as IUserModel;
+
+	if (!user.isModified("password")) {
+		return next();
+	}
+
+	const salt = await bcrypt.genSalt(config.saltRounds);
+	const hash = await bcrypt.hashSync(user.password, salt);
+	user.password = hash;
+	return next();
+});
+
+UserSchema.methods.comparePassword = async function (
+	checkPassword: string,
+): Promise<boolean> {
+	const user = this as IUserModel;
+
+	try {
+		const isValid = bcrypt.compare(checkPassword, user.password);
+		if (!isValid) {
+			return false;
+		}
+		return true;
+	} catch (error: any) {
+		return false;
+	}
+};
 
 export default mongoose.model<IUserModel>("User", UserSchema);
