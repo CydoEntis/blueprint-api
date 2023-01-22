2d15c69e25d3e74d0c0dd642a2500a7c09374c53
CydoEntis <cydoentis@gmail.com>
2023-01-22 15:29:20 -0500

    Added all basic crud operations to project routes

--- src/routes/project.routes.ts
+++ src/routes/project.routes.ts
@@ -3,6 +3,9 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/get/:projectId", controller.createProject);
 router.post("/create", controller.createProject);
+router.post("/update/:projectId", controller.createProject);
+router.post("/delete/:projectId", controller.createProject);
 
 export default router;
